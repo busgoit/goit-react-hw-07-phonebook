5ba3be57250daebcbc02d356ce95057750d8c8da
Vasil Karpliuk <bus.go.it@gmail.com>
2022-11-20 15:53:22 +0200

    fix loading

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,7 @@
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchContacts } from 'redux/operations';
-import { selectContacts } from 'redux/selectors';
+import { selectIsLoading, selectError } from 'redux/selectors';
 import Section from './Section';
 import Form from './Form';
 import Filter from './Filter';
@@ -10,7 +10,9 @@ import Contacts from './Contacts';
 export const App = () => {
   const dispatch = useDispatch();
 
-  const { isLoading, error } = useSelector(selectContacts);
+  const isLoading = useSelector(selectIsLoading);
+  const error = useSelector(selectError);
+  const isContacts = !isLoading && !error;
 
   useEffect(() => {
     dispatch(fetchContacts());
@@ -21,14 +23,14 @@ export const App = () => {
       <Section title="Phonebook">
         <Form />
       </Section>
-      <div>
-        {isLoading && <p>Loading contacts...</p>}
-        {error && <p>{error}</p>}
-      </div>
-      <Section title="Contacts">
-        <Filter />
-        <Contacts />
-      </Section>
+      {isLoading && <Section>Loading contacts...</Section>}
+      {error && <Section>{error}</Section>}
+      {isContacts && (
+        <Section title="Contacts">
+          <Filter />
+          <Contacts />
+        </Section>
+      )}
     </>
   );
 };
--- src/components/Contacts/Contacts.jsx
+++ src/components/Contacts/Contacts.jsx
@@ -14,6 +14,7 @@ import {
 const Contacts = () => {
   const contacts = useSelector(selectContacts);
   const filter = useSelector(selectFilter);
+
   const dispatch = useDispatch();
 
   const getFilteredContacts = () => {
